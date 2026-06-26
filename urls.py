00b3419d6c09d92c3314eb2339482cb399e08122
Nicolly Araujo <166459930+nnicolly@users.noreply.github.com>
2026-06-25 21:04:45 -0300

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path("livros/", views.listar_livros, name="listar_livros"),
-    path("autores/novo/", views.cadastrar_autor, name="cadastrar_autor"),
-]
