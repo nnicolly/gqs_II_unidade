061bb3eceb16f8dd094782ef1f42db97453ec304
camiilevitoriia <camile.vitoria345@gmail.com>
2026-06-25 21:15:40 -0300

    Teste 3 passou com sucesso

--- Gestao de qualidade de software/biblioteca/urls.py
+++ Gestao de qualidade de software/biblioteca/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("livros/", views.listar_livros, name="listar_livros"),
     path("autores/novo/", views.cadastrar_autor, name="cadastrar_autor"),
+    path("livros/cadastrar/", views.cadastrar_livro, name="cadastrar_livro"),
 ]
--- Gestao de qualidade de software/biblioteca/views.py
+++ Gestao de qualidade de software/biblioteca/views.py
@@ -25,3 +25,21 @@ def cadastrar_autor(request):
             return redirect("listar_livros")
 
     return render(request, "biblioteca/cadastrar_autor.html")
+
+def cadastrar_livro(request):
+    if request.method == "POST":
+        titulo_recebido = request.POST.get("titulo")
+        ano_recebido = request.POST.get("ano")
+        autor_id_recebido = request.POST.get("autor")
+        
+        autor_encontrado = Autor.objects.get(id=autor_id_recebido)
+        
+        Livro.objects.create(
+            titulo=titulo_recebido,
+            ano=ano_recebido,
+            autor=autor_encontrado,
+            disponivel=True
+        )
+        return redirect("listar_livros")
+        
+    return render(request, "biblioteca/cadastrar_livro.html")
